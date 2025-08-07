6bfcce1a99fed861865ddd17b88e577c01a52394
Srey Aem <sreyaem@Sreys-MacBook-Pro.local>
2025-08-08 00:29:49 +0700

    First commit

---
+++ src/services/api.js
@@ -0,0 +1,14 @@
+const API_KEY = "ed3c3017f1bca881a2e3bf16658a4140";
+const BASE_URL = "https://api.themoviedb.org/3";
+
+export const getPopularMovies = async () => {
+    const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+    const data = await response.json();
+    return data.results;
+};
+
+export const searchMovies = async (query) => {
+    const response = await fetch(`${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(query)}`);
+    const data = await response.json();
+    return data.results;
+};
